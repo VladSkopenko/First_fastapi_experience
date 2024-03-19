2e32392ff53b4aa95b08a77b89693db2c5280a13
VladSkopenko <skopirka2k17@gmail.com>
2024-03-19 04:26:04 +0300

    create new path

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/api/healthchecker")
 def root():
     return {"message": "Welcome to FastAPI!"}
+
+
+@app.get("/notes/{note_id}")
+async def read_note(note_id: int):
+    return {"note": note_id}
