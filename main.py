32c9f9ce2116e66b18991fb16361c16085bf3c5d
VladSkopenko <skopirka2k17@gmail.com>
2024-03-20 18:34:21 +0300

    full code main

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from db import get_db, Note
-from fastapi import FastAPI, Depends, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, Path, Query, Depends, HTTPException, status
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 
+from db import get_db, Note
+
 app = FastAPI()
+
+
+class ResponseNoteModel(BaseModel):
+    id: int = Field(default=1, ge=1)
+    name: str
+    description: str
+    done: bool
 
 
 @app.get("/api/healthchecker")
 def healthchecker(db: Session = Depends(get_db)):
     try:
-        # Make request
+        # Здійснюємо запит
         result = db.execute("SELECT 1").fetchone()
         if result is None:
             raise HTTPException(status_code=500, detail="Database is not configured correctly")
@@ -17,6 +25,22 @@ def healthchecker(db: Session = Depends(get_db)):
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail="Error connecting to the database")
+
+
+@app.get("/notes")
+async def read_notes(skip: int = 0, limit: int = Query(default=10, le=100, ge=10), db: Session = Depends(get_db)) -> \
+        list[ResponseNoteModel]:
+    notes = db.query(Note).offset(skip).limit(limit).all()
+    return notes
+
+
+@app.get("/notes/{note_id}", response_model=ResponseNoteModel)
+async def read_note(note_id: int = Path(description="The ID of the note to get", gt=0, le=10),
+                    db: Session = Depends(get_db)):
+    note = db.query(Note).filter(Note.id == note_id).first()
+    if note is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Not found')
+    return note
 
 
 class NoteModel(BaseModel):
