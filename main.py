4702ce076e348c8ddf9ea8ad1d79870380bd1526
VladSkopenko <skopirka2k17@gmail.com>
2024-03-20 17:48:35 +0300

    connect to db

--- main.py
+++ main.py
@@ -16,3 +16,8 @@ async def read_new_notes():
 @app.get("/notes/{note_id}")
 async def read_note(note_id: int = Path(description="The ID of the note to get", gt=0, le=10)):
     return {"note": note_id}
+
+@app.get("/notes")
+async def read_notes(skip: int = 0, limit: int = 10):
+    return {"message": f"Return all notes: skip: {skip}, limit: {limit}"}
+
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 This is just my poking at fastapi
+docker run --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=567234 -d postgres
